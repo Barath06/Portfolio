ff5e81b95ab65277fb5e23dafcd3ffe501c3a80d
Gokul Rao <64262019+gokul-369@users.noreply.github.com>
2020-10-08 20:39:38 +0530

    nothing much

--- file.js
+++ file.js
@@ -4,31 +4,30 @@ const links = document.querySelectorAll(".nav-links li");
 const actlink = document.querySelectorAll(".nav-links a");
 const nav = document.querySelector(".navbar");
 
-burger.addEventListener('click', () => {
-    navlinks.classList.toggle('open');
-    burger.classList.toggle('toggle');
-    links.forEach(link => {
-        link.classList.add("fade");
+burger.addEventListener("click", () => {
+  navlinks.classList.toggle("open");
+  burger.classList.toggle("toggle");
+  links.forEach((link) => {
+    link.classList.add("fade");
+  });
+  actlink.forEach((li) => {
+    li.addEventListener("click", () => {
+      navlinks.classList.remove("open");
+      burger.classList.remove("toggle");
+      links.forEach((link) => {
+        link.classList.remove("fade");
+      });
     });
-    actlink.forEach(li => {
-        li.addEventListener('click', () => {
-            navlinks.classList.remove('open');
-            burger.classList.remove('toggle');
-            links.forEach((link) => {
-              link.classList.remove("fade");
-            });
-        });
-    });
+  });
 });
 
 window.onscroll = function () {
-    var top = window.scrollY;
-    if (top >= 70) {
-        nav.classList.add("navcolorchange");
-        navlinks.style.backgroundColor = "#2F2E41";
-    }
-    else {
-        nav.classList.remove("navcolorchange");
-        navlinks.style.backgroundColor = "#1aa260";
-    }
+  var top = window.scrollY;
+  if (top >= 70) {
+    nav.classList.add("navcolorchange");
+    navlinks.style.backgroundColor = "#2F2E41";
+  } else {
+    nav.classList.remove("navcolorchange");
+    navlinks.style.backgroundColor = "#1aa260";
+  }
 };
