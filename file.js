9fc23b6e38684a5a2706c80791392bb9592526e5
Gokul Rao <64262019+gokul-369@users.noreply.github.com>
2020-10-08 13:29:48 +0530

    Add files via upload
    flight

---
+++ file.js
@@ -0,0 +1,34 @@
+const burger = document.querySelector(".burger");
+const navlinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+const actlink = document.querySelectorAll(".nav-links a");
+const nav = document.querySelector(".navbar");
+
+burger.addEventListener('click', () => {
+    navlinks.classList.toggle('open');
+    burger.classList.toggle('toggle');
+    links.forEach(link => {
+        link.classList.add("fade");
+    });
+    actlink.forEach(li => {
+        li.addEventListener('click', () => {
+            navlinks.classList.remove('open');
+            burger.classList.remove('toggle');
+            links.forEach((link) => {
+              link.classList.remove("fade");
+            });
+        });
+    });
+});
+
+window.onscroll = function () {
+    var top = window.scrollY;
+    if (top >= 50) {
+        nav.classList.add("navcolorchange");
+        navlinks.style.backgroundColor="lightcoral"
+    }
+    else {
+        nav.classList.remove("navcolorchange");
+        navlinks.style.backgroundColor = "#1aa260";
+    }
+};
