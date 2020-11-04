4e8bc935da21b7b3f29a766a7d1d927bb624d7e0
Gokul Rao <64262019+gokul-369@users.noreply.github.com>
2020-11-04 13:06:40 +0530

    modifications

---
+++ signup.js
@@ -0,0 +1,14 @@
+function verify() {
+    var pwd = document.getElementById("pwd");
+    var cpwd = document.getElementById("cpwd");
+  
+    if (pwd.value != cpwd.value) {
+      alert("Passwords doesn't match please make sure both are same");
+      pwd.style.borderBottom = "solid 2px red";
+      cpwd.style.borderBottom = "solid 2px red";
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
