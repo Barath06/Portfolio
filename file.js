eeb086f9a16aaf40b57eeb93eeabe639eecf919f
Gokul Rao <64262019+gokul-369@users.noreply.github.com>
2020-11-06 15:12:58 +0530

    major changes ,animations, backdrops

--- file.js
+++ file.js
@@ -1,18 +1,18 @@
 const burger = document.querySelector(".burger");
-const burgerdiv = document.querySelectorAll(".burger div");
 const navlinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 const actlink = document.querySelectorAll(".nav-links a");
 const nav = document.querySelector(".navbar");
-const logo = document.querySelector(".logo h4");
 const heading = document.querySelector(".homecontent h1");
 const para = document.querySelector(".homecontent p");
-const cbtn=document.querySelector(".homecontent button")
-window.addEventListener("load", () => {
-  heading.style.animation = "move 1s ease"
-  para.style.animation = "move 1.5s ease"
-  cbtn.style.animation = "move 1.8s ease"
+const cbtn = document.querySelector(".homecontent button");
+const abth2 = document.querySelector(".aboutcontent h2");
+const abtp = document.querySelector(".aboutcontent p");
 
+window.addEventListener("load", () => {
+  heading.style.animation = "move 1s ease";
+  para.style.animation = "move 1.5s ease";
+  cbtn.style.animation = "move 1.8s ease";
 });
 
 burger.addEventListener("click", () => {
@@ -22,7 +22,6 @@ burger.addEventListener("click", () => {
     link.classList.add("fade");
   });
   actlink.forEach((li) => {
-    li.style.color = "white";
     li.addEventListener("click", () => {
       navlinks.classList.remove("open");
       burger.classList.remove("toggle");
@@ -32,28 +31,16 @@ burger.addEventListener("click", () => {
     });
   });
 });
-
 window.onscroll = function () {
   var top = window.scrollY;
+  // console.log(top);
   if (top >= 70) {
     nav.classList.add("navcolorchange");
-    navlinks.classList.add("nav-links-colorchange");
-    logo.style.color = "white";
-    actlink.forEach((link) => {
-      link.style.color = "white";
-    });
-    burgerdiv.forEach((item) => {
-      item.classList.add("linecolorchange");
-    });
   } else {
     nav.classList.remove("navcolorchange");
-    navlinks.classList.remove("nav-links-colorchange");
-    logo.style.color = "#2f2e41";
-    burgerdiv.forEach((item) => {
-      item.classList.remove("linecolorchange");
-    });
-    actlink.forEach((link) => {
-      link.style.color = "#2f2e41";
-    });
+  }
+  if (top = 750) {
+    abth2.style.animation = "move 1.3s ease";
+    abtp.style.animation = "move 1.7s ease";
   }
 };
