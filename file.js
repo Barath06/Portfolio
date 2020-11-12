6ff66c08e85422fb40280fb6faa6fc012d0a074e
Gokul Rao <64262019+gokul-369@users.noreply.github.com>
2020-11-12 23:50:31 +0530

    major changes with sass, animations, look and feel

--- file.js
+++ file.js
@@ -3,17 +3,6 @@ const navlinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 const actlink = document.querySelectorAll(".nav-links a");
 const nav = document.querySelector(".navbar");
-const heading = document.querySelector(".homecontent h1");
-const para = document.querySelector(".homecontent p");
-const cbtn = document.querySelector(".homecontent button");
-const abth2 = document.querySelector(".aboutcontent h2");
-const abtp = document.querySelector(".aboutcontent p");
-
-window.addEventListener("load", () => {
-  heading.style.animation = "move 1s ease";
-  para.style.animation = "move 1.5s ease";
-  cbtn.style.animation = "move 1.8s ease";
-});
 
 burger.addEventListener("click", () => {
   navlinks.classList.toggle("open");
@@ -31,16 +20,13 @@ burger.addEventListener("click", () => {
     });
   });
 });
+
 window.onscroll = function () {
   var top = window.scrollY;
-  // console.log(top);
   if (top >= 70) {
     nav.classList.add("navcolorchange");
   } else {
     nav.classList.remove("navcolorchange");
   }
-  if (top = 750) {
-    abth2.style.animation = "move 1.3s ease";
-    abtp.style.animation = "move 1.7s ease";
-  }
 };
+
